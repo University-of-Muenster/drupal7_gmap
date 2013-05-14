a67553df999333bd98b0b08d47284f3199a22de9
Andriy Podanenko http://druler.com <podarok@ua.fm>
2013-05-14 14:53:34 +0300

    Issue #1994428 by podarok: CodeStyle fix.

--- js/directions.js
+++ js/directions.js
@@ -30,168 +30,179 @@
  */
 
 Drupal.gmap.addHandler('gmap', function (element) {
-  var obj = this;
+    var obj = this;
 
-  obj.bind('init', function () {
-    if ( obj.vars.behavior.addDirectionsPanel ) {
-      // try to recover the mapid : taken directly from gmap.js
-      var map = obj.map.getDiv();
-      var mapid = map.id.split('-');
-      if (Drupal.settings['gmap_remap_widgets']) {
-        if (Drupal.settings['gmap_remap_widgets'][obj.id]) {
-          jQuery.each(Drupal.settings['gmap_remap_widgets'][obj.id].classes, function() {
-            jQuery(obj).addClass(this);
-          });
-          mapid = Drupal.settings['gmap_remap_widgets'][obj.id].id.split('-');
+    obj.bind('init', function () {
+        if (obj.vars.behavior.addDirectionsPanel) {
+            // try to recover the mapid : taken directly from gmap.js
+            var map = obj.map.getDiv();
+            var mapid = map.id.split('-');
+            if (Drupal.settings['gmap_remap_widgets']) {
+                if (Drupal.settings['gmap_remap_widgets'][obj.id]) {
+                    jQuery.each(Drupal.settings['gmap_remap_widgets'][obj.id].classes, function () {
+                        jQuery(obj).addClass(this);
+                    });
+                    mapid = Drupal.settings['gmap_remap_widgets'][obj.id].id.split('-');
+                }
+            }
+            var instanceid = mapid.pop();
+            mapid.shift();
+            mapid = mapid.join('-');
+            // end of recover mapid
+
+            obj.directions = $('<div id="gmaps-' + mapid + '-directions0" class="gmap-control gmap-control-directions" />'); // this assumes that directions0 isn't taken already
+            $(map).after(obj.directions);
+            Drupal.gmap.setup.call(obj.directions[0])
         }
-      }
-      var instanceid = mapid.pop();
-      mapid.shift();
-      mapid = mapid.join('-');
-      // end of recover mapid
-
-      obj.directions = $( '<div id="gmaps-'+mapid+'-directions0" class="gmap-control gmap-control-directions" />' ); // this assumes that directions0 isn't taken already
-      $(map).after( obj.directions );
-      Drupal.gmap.setup.call( obj.directions[0] )
-    }
-  });
+    });
 
 });
 
 Drupal.gmap.addHandler('directions', function (element) {
-  var obj = this;
+    var obj = this;
 
-  obj.bind('ready', function () {
-    obj.directionsservice = new google.maps.DirectionsService();
-    obj.directionspanel = $(element);
+    obj.bind('ready', function () {
+        obj.directionsservice = new google.maps.DirectionsService();
+        obj.directionspanel = $(element);
 
-    if ( obj.vars && obj.vars.directions ) {
-      $.each( obj.vars.directions.items, function(directionsIndex,route) {
-        route.index = directionsIndex;
-        obj.change('destinationsdirections','all',route);
-      });
-    }
-
-  });
-
-  obj.bind('destinationsdirections', function(route) {
-    var directionsRequest = {waypoints:[]} // this has to match the google.maps.DirectionsRequest
-
-    if ( route.origin ) {
-      if ( route.origin.latitude ) { route.origin = new google.maps.LatLng( route.origin.latitude, route.origin.longitude ); }
-      directionsRequest.origin = route.origin;
-    }
-    $.each( route.waypoints, function( routeIndex , waypoint ) {
-      waypoint.waypointIndex = routeIndex;
-      if ( waypoint.latitude ) { waypoint.location = new google.maps.LatLng( waypoint.latitude, waypoint.longitude ); }
-      var requestWaypoint = {
-        location: waypoint.location,
-        stopover: true  // false would be better, but it makes the directions fail
-      }
-      $.extend( waypoint , requestWaypoint );
-      if ( !directionsRequest.origin ) { // if no origin was passed, then take the first point
-        directionsRequest.origin = waypoint.location;
-      }
-      else {
-        directionsRequest.waypoints.push( requestWaypoint );
-      }
-    });
-    if ( route.destination ) {
-      if ( route.destination.latitude ) { route.destination = new google.maps.LatLng( route.destination.latitude, route.destination.longitude ); }
-      directionsRequest.destination = route.destination;
-    }
-    else if ( directionsRequest.waypoints.length>0 ) {
-      directionsRequest.destination = directionsRequest.waypoints.pop().location;
-    } // if no destintion was passed, then take the last point
-    else {
-      route.message = Drupal.t('Not enough point');
-      route.status = false;
-      obj.change('directionsfail', 'all', route );
-    }
-
-    //parvietosanas veids
-    switch (route.type) {
-      case 1:
-        directionsRequest.travelMode = google.maps.TravelMode.WALKING;
-        break;
-      default:
-        directionsRequest.travelMode = google.maps.TravelMode.DRIVING;
-    }
-
-    if ( directionsRequest.waypoints.length == 0 ) { delete directionsRequest.waypoints; } // don't send waypoints if there aren't any
-    obj.directionsservice.route( directionsRequest , function( directions, status ) {
-      route.status = status;
-      if ( status == google.maps.DirectionsStatus.OK ) {
-        route.directions = directions;
-        obj.change('directionsSuccess', 'all', route );
-      }
-      else {
-        obj.change('directionsFail', 'all', route );
-      }
-    });
-
-  });
-  obj.bind('directionsSuccess', function(route) {
-    if ( !obj.vars.behavior.directionsCustomSuccess ) {
-      obj.change('directionsPanelInit','all',route);
-      obj.change('directionsPanelBoot','all',route);
-      obj.change('directionsPanelReady','all',route);
-    }
-  });
-
-  obj.bind('directionsPanelInit', function(route) {
-    if ( obj.vars.behavior.directionsPanelWrap ) {
-      route.panel = $('<div class="gmap-control-directionsPanel" /></div>');
-      if ( route.title ) { route.panel.append( '<h3 class="directions-title">'+route.title+'</h3>' ); }
-      obj.directionspanel.append( route.panel );
-    }
-    else {
-      route.panel = obj.directionspanel;
-    }
-  });
-  obj.bind('directionsPanelBoot', function(route) {
-    if ( !obj.vars.behavior.directionsCustomRenderer ) {
-      route.renderer = new google.maps.DirectionsRenderer({map: obj.map, panel:route.panel[0]});
-      google.maps.event.addListener( route.renderer , 'directions_changed' , function(event) {
-        obj.change('directionsPanelChanged','All',route);
-      });
-      route.renderer.setDirections( route.directions );
-    }
-  });
-
-  obj.bind('directionsPanelReady', function(route) {
-   /* Reformat the table to include some of the point titles */
-  /* @TODO */
-  });
-  obj.bind('directionsPanelReady', function(route) {
-    if ( obj.vars.behavior.directionsClickable ) {
-      route.obj = obj;
-      route.panel.bind( 'click', route , function(event) {
-        var obj = event.data.obj; // not sure if this is necessary
-        obj.change('directionsPanelClick','all',event.route);
-      });
-
-      if ( obj.vars.behavior.directionsClickableDefaultClicked ) {
-        route.panel.click();
-      }
-    }
-  });
-
-  obj.bind('directionsPanelClick', function(route) { // just do some class stuff for css
-    route.panel.toggleClass('directions-active');
-  });
-  obj.bind('directionsPanelClick', function(route) { // activate and deactivate the directions on the map
-    if ( obj.vars.behavior.directionsPanelActivateOnClick ) { // this behaviour may be undesirable
-      if ( panel.hasClass('directions-active') ) {;
-        if ( !obj.vars.behavior.directionsPanelActivateSingle && obj.map.data('activeDirectionsPanel') ) {
-          obj.map.data('activeDirectionsPanel').click();
+        if (obj.vars && obj.vars.directions) {
+            $.each(obj.vars.directions.items, function (directionsIndex, route) {
+                route.index = directionsIndex;
+                obj.change('destinationsdirections', 'all', route);
+            });
         }
-        route.renderer.setMap( obj.map );
-      }
-      else {
-        route.renderer.setMap( null );
-      }
-    }
-  });
+
+    });
+
+    obj.bind('destinationsdirections', function (route) {
+        var directionsRequest = {waypoints: []} // this has to match the google.maps.DirectionsRequest
+
+        if (route.origin) {
+            if (route.origin.latitude) {
+                route.origin = new google.maps.LatLng(route.origin.latitude, route.origin.longitude);
+            }
+            directionsRequest.origin = route.origin;
+        }
+        $.each(route.waypoints, function (routeIndex, waypoint) {
+            waypoint.waypointIndex = routeIndex;
+            if (waypoint.latitude) {
+                waypoint.location = new google.maps.LatLng(waypoint.latitude, waypoint.longitude);
+            }
+            var requestWaypoint = {
+                location: waypoint.location,
+                stopover: true  // false would be better, but it makes the directions fail
+            }
+            $.extend(waypoint, requestWaypoint);
+            if (!directionsRequest.origin) { // if no origin was passed, then take the first point
+                directionsRequest.origin = waypoint.location;
+            }
+            else {
+                directionsRequest.waypoints.push(requestWaypoint);
+            }
+        });
+        if (route.destination) {
+            if (route.destination.latitude) {
+                route.destination = new google.maps.LatLng(route.destination.latitude, route.destination.longitude);
+            }
+            directionsRequest.destination = route.destination;
+        }
+        else if (directionsRequest.waypoints.length > 0) {
+            directionsRequest.destination = directionsRequest.waypoints.pop().location;
+        } // if no destintion was passed, then take the last point
+        else {
+            route.message = Drupal.t('Not enough point');
+            route.status = false;
+            obj.change('directionsfail', 'all', route);
+        }
+
+        //parvietosanas veids
+        switch (route.type) {
+            case 1:
+                directionsRequest.travelMode = google.maps.TravelMode.WALKING;
+                break;
+            default:
+                directionsRequest.travelMode = google.maps.TravelMode.DRIVING;
+        }
+
+        if (directionsRequest.waypoints.length == 0) {
+            delete directionsRequest.waypoints;
+        } // don't send waypoints if there aren't any
+        obj.directionsservice.route(directionsRequest, function (directions, status) {
+            route.status = status;
+            if (status == google.maps.DirectionsStatus.OK) {
+                route.directions = directions;
+                obj.change('directionsSuccess', 'all', route);
+            }
+            else {
+                obj.change('directionsFail', 'all', route);
+            }
+        });
+
+    });
+    obj.bind('directionsSuccess', function (route) {
+        if (!obj.vars.behavior.directionsCustomSuccess) {
+            obj.change('directionsPanelInit', 'all', route);
+            obj.change('directionsPanelBoot', 'all', route);
+            obj.change('directionsPanelReady', 'all', route);
+        }
+    });
+
+    obj.bind('directionsPanelInit', function (route) {
+        if (obj.vars.behavior.directionsPanelWrap) {
+            route.panel = $('<div class="gmap-control-directionsPanel" /></div>');
+            if (route.title) {
+                route.panel.append('<h3 class="directions-title">' + route.title + '</h3>');
+            }
+            obj.directionspanel.append(route.panel);
+        }
+        else {
+            route.panel = obj.directionspanel;
+        }
+    });
+    obj.bind('directionsPanelBoot', function (route) {
+        if (!obj.vars.behavior.directionsCustomRenderer) {
+            route.renderer = new google.maps.DirectionsRenderer({map: obj.map, panel: route.panel[0]});
+            google.maps.event.addListener(route.renderer, 'directions_changed', function (event) {
+                obj.change('directionsPanelChanged', 'All', route);
+            });
+            route.renderer.setDirections(route.directions);
+        }
+    });
+
+    obj.bind('directionsPanelReady', function (route) {
+        /* Reformat the table to include some of the point titles */
+        /* @TODO */
+    });
+    obj.bind('directionsPanelReady', function (route) {
+        if (obj.vars.behavior.directionsClickable) {
+            route.obj = obj;
+            route.panel.bind('click', route, function (event) {
+                var obj = event.data.obj; // not sure if this is necessary
+                obj.change('directionsPanelClick', 'all', event.route);
+            });
+
+            if (obj.vars.behavior.directionsClickableDefaultClicked) {
+                route.panel.click();
+            }
+        }
+    });
+
+    obj.bind('directionsPanelClick', function (route) { // just do some class stuff for css
+        route.panel.toggleClass('directions-active');
+    });
+    obj.bind('directionsPanelClick', function (route) { // activate and deactivate the directions on the map
+        if (obj.vars.behavior.directionsPanelActivateOnClick) { // this behaviour may be undesirable
+            if (panel.hasClass('directions-active')) {
+                ;
+                if (!obj.vars.behavior.directionsPanelActivateSingle && obj.map.data('activeDirectionsPanel')) {
+                    obj.map.data('activeDirectionsPanel').click();
+                }
+                route.renderer.setMap(obj.map);
+            }
+            else {
+                route.renderer.setMap(null);
+            }
+        }
+    });
 
 });
--- js/highlight.js
+++ js/highlight.js
@@ -1,4 +1,3 @@
-
 /**
  * @file
  * Common marker highlighting routines.
@@ -40,139 +39,155 @@
  * }
  */
 
-Drupal.gmap.factory.highlight = function(options) {
-  return new google.maps.Circle(options);
+Drupal.gmap.factory.highlight = function (options) {
+    return new google.maps.Circle(options);
 }
 
 Drupal.gmap.addHandler('gmap', function (elem) {
-  var obj = this;
-  obj.highlights = {};
+    var obj = this;
+    obj.highlights = {};
 
-  var overlayHandler = function(map, highlight) {
-    this.setMap(map);
-    this.highlight = highlight;
-  }
-  overlayHandler.prototype = new google.maps.OverlayView();
-  overlayHandler.prototype.onAdd = function() {
-    var highlight = this.highlight;
-    if ( !highlight.opts ) { highlight.opts = {}; } // sanity
-    if ( !highlight.behavior ) { highlight.behavior = {}; } // sanity
-    if ( !highlight.position ) { highlight.position = new google.maps.LatLng( highlight.latitude, highlight.longitude ); } // if you have a pos already then use it, otherwise gimme a lat/lon
+    var overlayHandler = function (map, highlight) {
+        this.setMap(map);
+        this.highlight = highlight;
+    }
+    overlayHandler.prototype = new google.maps.OverlayView();
+    overlayHandler.prototype.onAdd = function () {
+        var highlight = this.highlight;
+        if (!highlight.opts) {
+            highlight.opts = {};
+        } // sanity
+        if (!highlight.behavior) {
+            highlight.behavior = {};
+        } // sanity
+        if (!highlight.position) {
+            highlight.position = new google.maps.LatLng(highlight.latitude, highlight.longitude);
+        } // if you have a pos already then use it, otherwise gimme a lat/lon
 
-    $.each({ // collect the options from either the highligh.opts object, from the passed target value, as a behavior or a default value
-        radius: {target:'radius',default:10}, // radius in pixels
-        strokeColor: {target:'border',default:'#777777'},
-        strokeWeight: {target:'weight',default:2},
-        strokeOpacity: {target:'opacity',default:0.7},
-        fillColor: {target:'color',default:'#777777'},
-        fillOpacity: {target:'opacity',default:0.7},
-        draggable: {behavior:'draggable',default:false},
-        editable: {behavior:'editable',default:false},
-      }, function( key , config ) {
-        if ( highlight.opts[key] ) { // options was passed in
-          return true;
+        $.each({ // collect the options from either the highligh.opts object, from the passed target value, as a behavior or a default value
+            radius: {target: 'radius', default: 10}, // radius in pixels
+            strokeColor: {target: 'border', default: '#777777'},
+            strokeWeight: {target: 'weight', default: 2},
+            strokeOpacity: {target: 'opacity', default: 0.7},
+            fillColor: {target: 'color', default: '#777777'},
+            fillOpacity: {target: 'opacity', default: 0.7},
+            draggable: {behavior: 'draggable', default: false},
+            editable: {behavior: 'editable', default: false},
+        }, function (key, config) {
+            if (highlight.opts[key]) { // options was passed in
+                return true;
+            }
+            else if (config.target && highlight[ config.target ]) { // highight.target should have a value
+                highlight.opts[key] = highlight[ config.target ];
+            }
+            else if (config.behavior && highlight.behavior && highlight.behavior[ config.behavior ]) { // value is a behaviour
+                highlight.opts[key] = highlight.behavior[ config.behavior ];
+            }
+            else if (config.default) { // default valuee
+                highlight.opts[key] = config.default;
+            }
+        });
+
+        highlight.opts.map = this.map;
+        highlight.opts.center = highlight.position;
+
+    }
+    overlayHandler.prototype.draw = function () {
+        var highlight = this.highlight;
+        if (!this.highlight.behavior.radiusInMeters) {
+            var projection = this.getProjection();
+            var mapZoom = this.map.getZoom();
+            var center = projection.fromLatLngToDivPixel(highlight.opts.center, mapZoom);
+            var radius = highlight.opts.radius;
+            var radial = projection.fromDivPixelToLatLng(new google.maps.Point(center.x, center.y + radius), mapZoom); // find a point that is the radius distance away in pixels
+            highlight.opts.radius = google.maps.geometry.spherical.computeDistanceBetween(highlight.opts.center, radial);
+            highlight.behavior.radiusInMeters = true;
         }
-        else if (config.target && highlight[ config.target ]) { // highight.target should have a value
-          highlight.opts[key] = highlight[ config.target ];
+        highlight.highlight = Drupal.gmap.factory.highlight(highlight.opts);
+    }
+    overlayHandler.prototype.onRemove = function () {
+        if (this.highlight.highlight) {
+            this.highlight.highlight.setMap(null);
         }
-        else if ( config.behavior && highlight.behavior && highlight.behavior[ config.behavior ] ) { // value is a behaviour
-          highlight.opts[key] = highlight.behavior[ config.behavior ];
+    }
+
+    Drupal.gmap.highlight = function (highlight) {
+        new overlayHandler(obj.map, highlight); // all actions happen in an overlayview, so that we can input pixels for radius instead of meters and positions
+    };
+    Drupal.gmap.unhighlight = function (highlight) { // this can take an object that has a highlight, or a highlight/circle object
+        if (highlight.highlight && highlight.highlight.setMap) {
+            highlight.highlight.setMap(null);
         }
-        else if (config.default) { // default valuee
-          highlight.opts[key] = config.default;
+        else if (highlight.setMap) {
+            highlight.setMap(null);
         }
-      });
-
-    highlight.opts.map = this.map;
-    highlight.opts.center = highlight.position;
-
-  }
-  overlayHandler.prototype.draw = function() {
-    var highlight = this.highlight;
-    if ( !this.highlight.behavior.radiusInMeters ) {
-      var projection = this.getProjection();
-      var mapZoom = this.map.getZoom();
-      var center = projection.fromLatLngToDivPixel(highlight.opts.center, mapZoom);
-      var radius = highlight.opts.radius;
-      var radial = projection.fromDivPixelToLatLng(new google.maps.Point(center.x, center.y+radius), mapZoom); // find a point that is the radius distance away in pixels
-      highlight.opts.radius = google.maps.geometry.spherical.computeDistanceBetween (highlight.opts.center, radial);
-      highlight.behavior.radiusInMeters = true;
     }
-    highlight.highlight = Drupal.gmap.factory.highlight(highlight.opts);
-  }
-  overlayHandler.prototype.onRemove = function() {
-    if ( this.highlight.highlight ) {
-      this.highlight.highlight.setMap( null );
-    }
-  }
 
-  Drupal.gmap.highlight = function (highlight) {
-    new overlayHandler(obj.map, highlight); // all actions happen in an overlayview, so that we can input pixels for radius instead of meters and positions
-  };
-  Drupal.gmap.unhighlight = function( highlight ) { // this can take an object that has a highlight, or a highlight/circle object
-    if ( highlight.highlight && highlight.highlight.setMap ) {
-      highlight.highlight.setMap( null );
-    }
-    else if ( highlight.setMap ) {
-      highlight.setMap( null );
-    }
-  }
+    // set and remove map highlights
 
-  // set and remove map highlights
+    obj.bind('addHighlight', function (highlight) {
+        Drupal.gmap.highlight(highlight);
+    });
+    obj.bind('removeHighlight', function (highlight) {
+        Drupal.gmap.unhighlight(highlight);
+    });
 
-  obj.bind('addHighlight', function(highlight) {
-    Drupal.gmap.highlight( highlight );
-  });
-  obj.bind('removeHighlight', function(highlight) {
-    Drupal.gmap.unhighlight( highlight );
-  });
+    // Marker specific code:
+    var activeMarker; // remember that last marker activated.  In the default case we only allow one highlighted marker at a time
+    obj.bind('markerHighlight', function (marker) {
+        if (activeMarker && !obj.vars.behavior.allowMultipleMarkerHighlight) {
+            obj.change('markerUnHighlight', -1, activeMarker);
+        } // deactivate the active marker
 
- // Marker specific code:
-  var activeMarker; // remember that last marker activated.  In the default case we only allow one highlighted marker at a time
-  obj.bind('markerHighlight', function (marker) {
-    if ( activeMarker && !obj.vars.behavior.allowMultipleMarkerHighlight ) { obj.change('markerUnHighlight',-1, activeMarker); } // deactivate the active marker
+        // If the highlight arg option is used in views highlight the marker.
+        if (!marker.highlight) {
+            marker.highlight = {}
+        }
+        if (!marker.highlight.color && obj.vars.styles.highlight_color) {
+            marker.highlight.color = '#' + obj.vars.styles.highlight_color;
+        }
+        marker.highlight.position = marker.marker.getPosition();
+        Drupal.gmap.highlight(marker.highlight);
+        activeMarker = marker;
+    });
+    obj.bind('markerUnHighlight', function (marker) {
+        if (!marker) {
+            marker = activeMarker;
+        } // remove the active marker if no marker is passed in
+        if (activeMarker === marker) {
+            activeMarker = null;
+        }
+        if (marker.highlight) {
+            Drupal.gmap.unhighlight(marker.highlight);
+        }
+    });
 
-    // If the highlight arg option is used in views highlight the marker.
-    if ( !marker.highlight ) { marker.highlight = {} }
-    if ( !marker.highlight.color && obj.vars.styles.highlight_color ) { marker.highlight.color = '#' + obj.vars.styles.highlight_color; }
-    marker.highlight.position = marker.marker.getPosition();
-    Drupal.gmap.highlight( marker.highlight );
-    activeMarker = marker;
-  });
-  obj.bind('markerUnHighlight', function(marker) {
-    if ( !marker ) { marker = activeMarker; } // remove the active marker if no marker is passed in
-    if ( activeMarker === marker ) { activeMarker = null; }
-    if ( marker.highlight ) {
-      Drupal.gmap.unhighlight(marker.highlight);
-    }
-  });
+    /**
+     * Marker Binds
+     *
+     * Marker highlight code has been moved to this file from the marker.js
+     *
+     * Note that we rely on the obj.vars.behavior.highlight var to
+     * decide if should highlight markers on events.
+     * This decision could be made as an outer if conditional, instead
+     * of repeated inside each bind, but this arrangement allows for
+     * the behaviour to change, at a small cost.
+     */
 
-  /**
-   * Marker Binds
-   *
-   * Marker highlight code has been moved to this file from the marker.js
-   *
-   * Note that we rely on the obj.vars.behavior.highlight var to
-   * decide if should highlight markers on events.
-   * This decision could be made as an outer if conditional, instead
-   * of repeated inside each bind, but this arrangement allows for
-   * the behaviour to change, at a small cost.
-   */
-
-  obj.bind('addmarker', function (marker) {
-    if (obj.vars.behavior.highlight) {
-      google.maps.event.addListener(marker.marker, 'mouseover', function () {
-        obj.change('markerHighlight',-1,marker);
-      });
-      google.maps.event.addListener(marker.marker, 'mouseout', function () {
-        obj.change('markerUnHighlight',-1,marker);
-      });
-    }
-    // If the highlight arg option is used in views highlight the marker.
-    if (marker.opts.highlight == 1) {
-      obj.change('markerHighlight',-1,marker);
-    }
-  });
+    obj.bind('addmarker', function (marker) {
+        if (obj.vars.behavior.highlight) {
+            google.maps.event.addListener(marker.marker, 'mouseover', function () {
+                obj.change('markerHighlight', -1, marker);
+            });
+            google.maps.event.addListener(marker.marker, 'mouseout', function () {
+                obj.change('markerUnHighlight', -1, marker);
+            });
+        }
+        // If the highlight arg option is used in views highlight the marker.
+        if (marker.opts.highlight == 1) {
+            obj.change('markerHighlight', -1, marker);
+        }
+    });
 
 // Originally I moved mouse highlights to the extra event binds before I realized that there is likely a usecase for highlights without enabling extra events
 //   obj.bind('mouseovermarker', function(marker) {
